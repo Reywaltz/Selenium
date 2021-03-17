22c5bdf8bffd91e98c497c6df0ee87e9b920fea3
Reywaltz <george1147@yandex.ru>
2021-03-17 20:15:41 +0300

    Добавлен метод на получения сообщений + документация

--- lab_4/internal/browser/browser.py
+++ lab_4/internal/browser/browser.py
@@ -1,10 +1,8 @@
 import time
 from dataclasses import dataclass
 from logging import Logger
-from typing import List
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 @dataclass
@@ -12,16 +10,31 @@ class WebDriver:
     logger: Logger
     driver: webdriver.Chrome
 
-    def login(self, login, password):
+    def login(self, login: str, password: str):
+        """Метод авторизация в ВК
+
+        :param login: Логин пользователя
+        :type login: str
+        :param password: Пароль пользователя
+        :type password: str
+        """
         login_field = self.driver.find_element_by_id('index_email')
         password_field = self.driver.find_element_by_id('index_pass')
         login_field.send_keys(login)
         password_field.send_keys(password)
 
         self.driver.find_element_by_id('index_login_button').click()
-
+        self.logger.info("Успешный вход")
 
     def get_dialog(self):
+        """Метод получения диалогов"""
         self.driver.find_element_by_xpath('//*[@id="l_msg"]/a/span[1]').click()
+        self.logger.info("Переход в диалоги успешен")
+        time.sleep(2)
 
-        self.driver.find_element_by_xpath('//*[@id="im_dialogs"]/div[1]/div[1]/div/div[1]/li[5]')
+    def get_last_messages(self):
+        """Метод получения сообщений"""
+        messages = self.driver.find_elements_by_class_name("nim-dialog--content")
+        self.logger.info("Получены последние 10 сообщений")
+        for i in messages[:10]:
+            print(i.text.split('\n'), end='\n')
--- lab_4/main.py
+++ lab_4/main.py
@@ -1,16 +1,19 @@
+import time
+
+import toml
+from selenium import webdriver
+
 from internal.browser import browser
 from pkg import logger
-from selenium import webdriver
-import toml
 
 # TODO
-#Открыть Вконтакте. 
+# Открыть Вконтакте.
 # Выполнить вход и получить список из 10 последних сообщений.
 
-URL = 'https://vk.com'
 
 cfg = toml.load('cfg.toml')
 
+URL = cfg.get('vk').get('URL')
 email = cfg.get('vk').get('email')
 password = cfg.get('vk').get('password')
 
@@ -24,4 +27,6 @@ wd = browser.WebDriver(log, chrome_driver)
 if __name__ == '__main__':
     wd.driver.get(URL)
     wd.login(email, password)
-    # wd.empty_press()
+    time.sleep(5)
+    wd.get_dialog()
+    wd.get_last_messages()
